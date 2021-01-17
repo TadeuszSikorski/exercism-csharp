b0becdb83ad9c398a3281c7a0d10c473a7c5bccf
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-01-17 20:58:42 +0100

    solution of isogram exercise

---
+++ isogram/Isogram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word) => 
+        word.ToLower()
+            .Where(Char.IsLetter)
+            .GroupBy(letter => letter)
+            .All(letter => letter.Count() == 1);
+}
