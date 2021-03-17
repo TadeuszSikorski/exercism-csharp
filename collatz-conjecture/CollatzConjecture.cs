3ed7b152c1d6d72ed3d0d64ac9f3f3f6ae505155
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-03-17 18:34:01 +0100

    solution of collatz-conjecture exercise

---
+++ collatz-conjecture/CollatzConjecture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int number)
+    {
+        if (number < 1)
+            throw new ArgumentOutOfRangeException();
+
+        if (number == 1)
+            return 0;
+
+        int counter = 0;
+            
+        while(number != 1)
+        {
+            number = (number % 2 == 0) ? number /= 2 : number = (3 * number) + 1; 
+            counter ++;
+        }
+        
+        return counter;
+    }
+
+
+}
