f15ce7340a18a540e5603c483ab1908ddcf8eb51
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-03-18 08:44:41 +0100

    update acronym solution

---
+++ acronym/Acronym.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase) {
+        char[] separators = new char[] { ' ', '-', '_' };
+        var words = phrase.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+        return new string(words.Select(x => char.Parse(x.Substring(0, 1).ToUpper())).ToArray());
+    }
+}
