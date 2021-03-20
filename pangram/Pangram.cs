e32d1f4a3db51d6a0df6e3c9180baa922931571c
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-03-20 13:14:15 +0100

    solution of pangram exercise

---
+++ pangram/Pangram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Pangram
+{
+    private const int AlphabetSize = 26;
+
+    public static bool IsPangram(string input) => input
+                    .Select(character => character.ToString())
+                    .Where(letter => Char.IsLetter(letter.ElementAt(0)))
+                    .ToHashSet(StringComparer.OrdinalIgnoreCase).Count() == AlphabetSize;
+}
