5fe2847191766c414f29c4fd86dcfd4faf4bc7d9
TadeuszSikorski <sikorski.tadeusz@yahoo.com>
2021-04-16 14:21:19 +0200

    solution of grains exercise

---
+++ grains/Grains.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class Grains
+{
+    public static ulong Square(int n) => 
+          n < 1 || n > 64 
+        ? throw new ArgumentOutOfRangeException() 
+        : (ulong) Math.Pow(2, n - 1);
+
+    public static ulong Total() => (ulong) Math.Pow(2, 64) - 1;
+}
